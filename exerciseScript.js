6e91a2179b5bd03ce05e6e88637f0ba6f8c84767
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-10-27 11:59:58 +0100

    updated format ex 1

--- exerciseScript.js
+++ exerciseScript.js
@@ -1,9 +1,16 @@
+//EX 1
 function getDateAndTime()
 {
     var str = "";
+
+    var days = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
     var now = new Date();
-    str = now.toDateString() + ' ' + now.toLocaleTimeString();
+    str = days[now.getDay()] + ' ' + now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
     document.getElementById('getDate').innerHTML = str;
 }
 
 setInterval(getDateAndTime, 1000);
+
+//EX 2
+
