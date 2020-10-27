b154090a12187b298b60ef1cf9ba4eec236c2d31
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-10-27 13:30:52 +0100

    trubbel öv.2

--- exerciseScript.js
+++ exerciseScript.js
@@ -1,16 +1,36 @@
 //EX 1
-function getDateAndTime()
-{
-    var str = "";
+function getDateAndTime() {
+  var str = "";
 
-    var days = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+  var days = new Array(
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  );
 
-    var now = new Date();
-    str = days[now.getDay()] + ' ' + now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
-    document.getElementById('getDate').innerHTML = str;
+  var now = new Date();
+  str =
+    days[now.getDay()] +
+    " " +
+    now.toLocaleDateString() +
+    " " +
+    now.toLocaleTimeString();
+    document.getElementById("getDate").innerHTML = str;
+    setInterval(getDateAndTime, 1000);
 }
 
-setInterval(getDateAndTime, 1000);
 
 //EX 2
+function isItALeapYear() {
+  var year = document.getElementById("myYear").value;
+  var yes = "This is a leap year.";
+  var no = "This is not a leap year.";
 
+  if (year % 400 === 0) document.getElementById("leapYear").innerHTML = yes;
+  if (year % 100 === 0) document.getElementById("leapYear").innerHTML = no;
+  return year % 4 === 0;
+}
